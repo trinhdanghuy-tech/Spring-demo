534523a0d0d9c0e1254dfd1e1c72623c1dfeac6c
trinhdanghuy-tech <trinhdanghuy29@gmail.com>
2026-03-24 23:18:06 +0700

    thêm update and delete product

--- src/main/java/vn/trinhdanghuy/laptopvn/controller/admin/ProductController.java
+++ src/main/java/vn/trinhdanghuy/laptopvn/controller/admin/ProductController.java
@@ -5,15 +5,15 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.fasterxml.jackson.annotation.JsonCreator.Mode;
-
 import jakarta.validation.Valid;
 
 import vn.trinhdanghuy.laptopvn.domain.Product;
+import vn.trinhdanghuy.laptopvn.domain.User;
 import vn.trinhdanghuy.laptopvn.services.ProductService;
 import vn.trinhdanghuy.laptopvn.services.UploadService;
 import java.util.List;
@@ -60,6 +60,46 @@ public class ProductController {
             return "admin/product/create";
         }
         return "redirect:/admin/product";
+    }
+
+    /* Update product */
+    @GetMapping("/admin/product/edit/{id}")
+    public String getUpdateProductPage(Model model, @PathVariable("id") long id) {
+        Product currentProduct = this.productService.getProductById(id);
+        if (currentProduct == null) {
+            return "redirect:/admin/product";
+        }
+        model.addAttribute("newProduct", currentProduct);
+        return "admin/product/update";
+    }
+
+    @PostMapping("/admin/product/edit")
+    public String postUpdateProduct(Model model, @ModelAttribute("newProduct") Product product) {
+        Product currentProduct = this.productService.getProductById(product.getId());
+        if (currentProduct != null) {
+            currentProduct.setPrice(product.getPrice());
+            currentProduct.setQuantity(product.getQuantity());
+            currentProduct.setDetailDesc(product.getDetailDesc());
+            currentProduct.setShortDesc(product.getShortDesc());
+            this.productService.handleSaveProduct(currentProduct);
+        }
+        return "redirect:/admin/product";
+    }
+
+    // Hiển thị trang confirm delete
+    @GetMapping("/admin/product/delete/{id}")
+    public String getDeleteProductPage(@PathVariable("id") long id, Model model) {
+        model.addAttribute("id", id);
+        model.addAttribute("newProduct", new Product());
+        return "admin/product/delete"; // JSP confirm delete
+    }
+
+    // Xử lý POST khi nhấn nút Delete
+    @PostMapping("/admin/product/delete")
+    public String postDeleteProduct(@ModelAttribute("newProduct") Product product) {
+        // Xóa product theo id
+        productService.deleteProductById(product.getId());
+        return "redirect:/admin/product"; // Quay về trang danh sách
     }
 
 }
