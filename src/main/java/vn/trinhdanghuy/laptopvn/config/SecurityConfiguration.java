fb5b4de3febaba68b309c36949a6cf031ba9ea21
trinhdanghuy-tech <trinhdanghuy29@gmail.com>
2026-03-29 16:22:24 +0700

    feat: implement Spring Security configuration with custom authentication, session management, and access control rules

--- src/main/java/vn/trinhdanghuy/laptopvn/config/SecurityConfiguration.java
+++ src/main/java/vn/trinhdanghuy/laptopvn/config/SecurityConfiguration.java
@@ -21,78 +21,81 @@ import vn.trinhdanghuy.laptopvn.services.validator.CustomUserDetailsService;
 @EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfiguration {
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+                return new BCryptPasswordEncoder();
+        }
 
-    @Bean
-    public UserDetailsService userDetailsService(UserService userService) {
-        return new CustomUserDetailsService(userService);
-    }
+        @Bean
+        public UserDetailsService userDetailsService(UserService userService) {
+                return new CustomUserDetailsService(userService);
+        }
 
-    @Bean
-    public CustomSuccessHandler customSuccessHandler() {
-        return new CustomSuccessHandler();
-    }
+        @Bean
+        public CustomSuccessHandler customSuccessHandler() {
+                return new CustomSuccessHandler();
+        }
 
-    @Bean
-    public DaoAuthenticationProvider authProvider(
-            PasswordEncoder passwordEncoder,
-            UserDetailsService userDetailsService) {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userDetailsService);
-        authProvider.setPasswordEncoder(passwordEncoder);
-        authProvider.setHideUserNotFoundExceptions(false);
-        return authProvider;
-    }
+        @Bean
+        public DaoAuthenticationProvider authProvider(
+                        PasswordEncoder passwordEncoder,
+                        UserDetailsService userDetailsService) {
+                DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+                authProvider.setUserDetailsService(userDetailsService);
+                authProvider.setPasswordEncoder(passwordEncoder);
+                authProvider.setHideUserNotFoundExceptions(false);
+                return authProvider;
+        }
 
-    @Bean
-    public SpringSessionRememberMeServices rememberMeServices() {
-        SpringSessionRememberMeServices rememberMeServices = new SpringSessionRememberMeServices();
-        // optionally customize
-        rememberMeServices.setAlwaysRemember(true);
-        return rememberMeServices;
-    }
+        @Bean
+        public SpringSessionRememberMeServices rememberMeServices() {
+                SpringSessionRememberMeServices rememberMeServices = new SpringSessionRememberMeServices();
+                // optionally customize
+                rememberMeServices.setAlwaysRemember(true);
+                return rememberMeServices;
+        }
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        // Allow internal forwards/errors t o JSP views, otherwise /login forwards back
-                        // into security.
-                        .dispatcherTypeMatchers(DispatcherType.FORWARD, DispatcherType.INCLUDE, DispatcherType.ERROR)
-                        .permitAll()
-                        .requestMatchers("/", "/login", "/register", "/error", "/client/**", "/css/**", "/js/**",
-                                "/images/**", "/products/**", "/product/**")
-                        .permitAll()
-                        // Role admin
-                        .requestMatchers("/admin/**").hasRole("ADMIN")
-                        .requestMatchers("/cart", "/cart/**").hasAnyRole("USER", "ADMIN")
-                        .anyRequest().authenticated())
-                .sessionManagement((sessionManagement) -> sessionManagement
-                        .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
-                        .invalidSessionUrl("/logout?expired")
-                        .maximumSessions(1)
-                        .maxSessionsPreventsLogin(false))
-                .rememberMe(rememberMe -> rememberMe
-                        .rememberMeServices(rememberMeServices()))
-                .formLogin(form -> form
-                        .loginPage("/login")
-                        .loginProcessingUrl("/login")
-                        .successHandler(customSuccessHandler())
-                        .failureUrl("/login?error")
-                        .permitAll())
-                .exceptionHandling(ex -> ex.accessDeniedPage(
-                        "/access-denied"))
-                .logout(logout -> logout
-                        .logoutUrl("/logout")
-                        .logoutSuccessUrl("/login?logout")
-                        .invalidateHttpSession(true)
-                        .clearAuthentication(true)
-                        .deleteCookies("JSESSIONID")
-                        .permitAll());
-        return http.build();
-    }
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+                http
+                                .csrf(csrf -> csrf.disable())
+                                .authorizeHttpRequests(auth -> auth
+                                                // Allow internal forwards/errors t o JSP views, otherwise /login
+                                                // forwards back
+                                                // into security.
+                                                .dispatcherTypeMatchers(DispatcherType.FORWARD, DispatcherType.INCLUDE,
+                                                                DispatcherType.ERROR)
+                                                .permitAll()
+                                                .requestMatchers("/", "/login", "/register", "/error", "/client/**",
+                                                                "/css/**", "/js/**",
+                                                                "/images/**", "/products/**", "/product/**", "/cart",
+                                                                "/cart/**")
+                                                .permitAll()
+                                                // Role admin
+                                                .requestMatchers("/admin/**").hasRole("ADMIN")
+                                                .anyRequest().authenticated())
+                                .sessionManagement((sessionManagement) -> sessionManagement
+                                                .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
+                                                .invalidSessionUrl("/logout?expired")
+                                                .maximumSessions(1)
+                                                .maxSessionsPreventsLogin(false))
+                                .rememberMe(rememberMe -> rememberMe
+                                                .rememberMeServices(rememberMeServices()))
+                                .formLogin(form -> form
+                                                .loginPage("/login")
+                                                .loginProcessingUrl("/login")
+                                                .successHandler(customSuccessHandler())
+                                                .failureUrl("/login?error")
+                                                .permitAll())
+                                .exceptionHandling(ex -> ex.accessDeniedPage(
+                                                "/access-denied"))
+                                .logout(logout -> logout
+                                                .logoutUrl("/logout")
+                                                .logoutSuccessUrl("/login?logout")
+                                                .invalidateHttpSession(true)
+                                                .clearAuthentication(true)
+                                                .deleteCookies("JSESSIONID")
+                                                .permitAll());
+                return http.build();
+        }
 }
